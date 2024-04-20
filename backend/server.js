be3df2aa60a3c679977ddc78c004ab0ded1554b4
Shahid Raza <shahidraza02qwert@gmail.com>
2024-04-20 17:21:58 +0530

    prod ready

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import mongoose, { connect } from 'mongoose';
+import cors from "cors"
 import path from 'path';
+
 const app=express();
 const port= process.env.PORT || 8080
 const url=process.env.DB_URL
+
+if (process.env.NODE_ENV.trim() === "production") {
+  app.use(express.static(path.join(path.resolve(), 'frontend/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(path.resolve(), 'frontend/build', 'index.html'));
+  });
+  app.use(cors())
+}
 const ConnectDB= ()=>{
     try {
          return new Promise(async(resolve,reject)=>{
@@ -26,11 +37,11 @@ app.get('/list',(req,res)=>{
     return res.status(200).json({message:"hii, how are you",success:'true'})
 })
 
-app.use(express.static(path.join(path.resolve(),"frontend","dist")))
+// app.use(express.static(path.join(path.resolve(),"frontend","dist")))
 
-app.get("*", (req, res) => {
-    return res.sendFile(path.join(path.resolve(), "frontend", "dist", "index.html"));
-  });
+// app.get("*", (req, res) => {
+//     return res.sendFile(path.join(path.resolve(), "frontend", "dist", "index.html"));
+//   });
 
 
 ConnectDB().then((connect)=>{
